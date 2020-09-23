40b6872b9c2478b1b43df0ccaaef78ce09b5afa5
Heart 3DS <narongrit@3dsinteractive.com>
2020-09-23 16:47:47 +0700

    Change init to public

--- Sources/CarBode/BarcodeData.swift
+++ Sources/CarBode/BarcodeData.swift
@@ -5,7 +5,7 @@ public struct BarcodeData {
     public let value: String
     public let type: AVMetadataObject.ObjectType
     
-    init(value: String, type: AVMetadataObject.ObjectType){
+    public init(value: String, type: AVMetadataObject.ObjectType){
         self.value = value
         self.type = type
     }
