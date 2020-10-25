83034fc63e5faf201fa4122b1a13fdc1efb3148d
Heart Narongrit <narongrit@3dsinteractive.com>
2020-10-26 00:18:22 +0700

    Add onBarcodeGenerated

--- ExampleProject/ExampleProject/ModalBarcodeGenerator.swift
+++ ExampleProject/ExampleProject/ModalBarcodeGenerator.swift
@@ -14,14 +14,28 @@ struct ModalBarcodeGenerator: View {
     @State var dataString = "Hello Carbode"
     @State var barcodeType = CBBarcodeView.BarcodeType.qrCode
     @State var rotate = CBBarcodeView.Orientation.up
+    
+    @State var barcodeImage: UIImage?
 
     var body: some View {
         VStack {
             CBBarcodeView(data: $dataString,
                 barcodeType: $barcodeType,
                 orientation: $rotate)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 400, maxHeight: 400, alignment: .topLeading)
-
+                { image in
+                    self.barcodeImage = image
+                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 400, maxHeight: 400, alignment: .topLeading)
+            
+            Spacer()
+            Button(action: {
+                if let barcodeImage = self.barcodeImage {
+                    print("Image Size = \(barcodeImage.size.width) x \(barcodeImage.size.height)")
+                }
+            }) {
+                Text("Print BarcodeSize to output")
+            }
+            Spacer()
+            
             HStack {
 
                 Spacer()
