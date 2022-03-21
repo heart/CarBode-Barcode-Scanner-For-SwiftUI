a1b3689d8da5ebf4e76e76663a25107432dc2139
Licardo <1014660822@qq.com>
2022-03-21 12:01:51 +0800

    fix "only available in 14.0 or newer" issue

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CarBode",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
